d20460e14ce3bbcbb21f78fa456e6de5857cbfdd
G5KoNoah <noah.hilaire@outlook.fr>
2026-01-17 18:47:23 +0100

    Ajout de la page d'accueil sans informations

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
+
+const projects = document.querySelectorAll(".project");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }else{
+        entry.target.classList.remove("visible")
+      }
+    });
+  },
+  { threshold: 0.3 }
+);
+
+projects.forEach(project => observer.observe(project));
+
+// Filtrage par langage
+const buttons = document.querySelectorAll(".filters button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    buttons.forEach(b => b.classList.remove("active"));
+    button.classList.add("active");
+
+    const filter = button.dataset.filter;
+
+    projects.forEach(project => {
+      if (filter === "all" || project.dataset.lang === filter) {
+        project.style.display = "block";
+      } else {
+        project.style.display = "none";
+      }
+    });
+  });
+});
+
+
+
+
+
+
--- script.js
+++
@@ -1,3 +0,0 @@
-// Script vide pour le moment
-// Tu pourras l'utiliser plus tard pour des animations ou interactions
-console.log("Portfolio chargé");
